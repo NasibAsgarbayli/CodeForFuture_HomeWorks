d7fc73ac12cfb2823be4afd3c184774aa136ecaa
NasibAsgarbayli <asgarbaylinasib@gmail.com>
2025-10-14 00:39:19 +0400

    Clean up .vs and build artifacts

--- Tasks/CodeForFuture_Tasks/CodeForFuture_Tasks/Program.cs
+++ Tasks/CodeForFuture_Tasks/CodeForFuture_Tasks/Program.cs
@@ -1,42 +1,46 @@
 ﻿
 #region Verilmis n tam ededinin sade ve ya murekkeb oldugunu tapin.
-Console.Write("Ededi daxil edin: ");
+using System.Text;
 
-int n = Convert.ToInt32(Console.ReadLine());
-bool isPrime = true;
+//Console.Write("Ededi daxil edin: ");
 
-if (n == 0 || n == 1)
-{
-    Console.WriteLine($"{n} ne sade, ne de mürekkeb ededdir");
-}
-else
-{
-    if (n < 0)
-    {
-        Console.WriteLine("Menfi ededler üçün sadelik yoxlanmır");
-    }
-    else
-    {
-        for (int i = 2; i * i <= n; i++)
-        {
-            if (n % i == 0)
-            {
-                isPrime = false;
-                break;
-            }
-        }
+//int n = Convert.ToInt32(Console.ReadLine());
+//bool isPrime = true;
 
-        if (isPrime)
-        {
-            Console.WriteLine($"{n} ededi sade ededdir");
-        }
-        else
-        {
-            Console.WriteLine($"{n} ededi mürekkeb ededdir");
-        }
-    }
-}
+//if (n == 0 || n == 1)
+//{
+//    Console.WriteLine($"{n} ne sade, ne de mürekkeb ededdir");
+//}
+//else
+//{
+//    if (n < 0)
+//    {
+//        Console.WriteLine("Menfi ededler üçün sadelik yoxlanmır");
+//    }
+//    else
+//    {
+//        for (int i = 2; i * i <= n; i++)
+//        {
+//            if (n % i == 0)
+//            {
+//                isPrime = false;
+//                break;
+//            }
+//        }
 
-//salam
+//        if (isPrime)
+//        {
+//            Console.WriteLine($"{n} ededi sade ededdir");
+//        }
+//        else
+//        {
+//            Console.WriteLine($"{n} ededi mürekkeb ededdir");
+//        }
+//    }
+//}
+
+
 #endregion
+
+
 
